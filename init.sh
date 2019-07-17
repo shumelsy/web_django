44e5a1d5e71d69b9d643909217a09bf623bb3677
shumelsy <sveta@shumel.ru>
2019-07-17 23:19:35 +0500

    some bug fixes

--- init.sh
+++ init.sh
@@ -1,5 +1,11 @@
+mv web_django/web ./
+mv web_django/init.sh ./
+rm -rf web_django
+
 sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
+sudo unlink /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
+
 sudo ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
 sudo /etc/init.d/gunicorn restart
 sudo /etc/init.d/mysql start
