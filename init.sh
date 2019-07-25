6fab22dceaa470b876d7870f9b518f934cf349cd
shumelsy <sveta@shumel.ru>
2019-07-25 08:21:22 +0500

    Some improves

--- init.sh
+++ init.sh
@@ -2,6 +2,8 @@ mv web_django/web ./
 mv web_django/init.sh ./
 rm -rf web_django
 
+chmod +x init.sh
+
 sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo unlink /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
