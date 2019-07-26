68c040235bc4a7d55e2499a307ea308ec0077f61
shumelsy <sveta@shumel.ru>
2019-07-26 06:29:52 +0500

    Some improves

--- web/hello.py
+++ web/hello.py
@@ -1,9 +1,9 @@
 def wsgi_application(environ, start_response):
         status = '200 OK'
         headers = [
-                ('Content-Type', 'text/plain')
+                ('text/plain')
         ]
-        body = [bites(i + '\n', 'ascii') for i in environ['QUERY_STRING'].split('&')]
+        body = [bytes(i + '\n', 'ascii') for i in environ['QUERY_STRING'].split('&')]
         start_response(status, headers)
         return body
 
