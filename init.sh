944d80cb71057a240e9b71169948930e2ab50105
shumelsy <sveta@shumel.ru>
2019-07-28 10:40:29 +0500

    Some improves

--- init.sh
+++ init.sh
@@ -10,10 +10,11 @@ sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo unlink /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 
-sudo ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
+#sudo ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
 #sudo /etc/init.d/gunicorn restart
 #sudo /etc/init.d/gunicorn stop
 cd /home/box/web/etc/
-gunicorn --bind='0.0.0.0:8080' hello:app
+#sudo gunicorn -c gunicorn.conf hello:app
+sudo gunicorn --bind='0.0.0.0:8080' hello:app
 
 sudo /etc/init.d/mysql start
