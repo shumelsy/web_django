b4226c829ffeb5f204d00e1c0fb642d51834cff8
shumelsy <sveta@shumel.ru>
2019-07-28 10:46:05 +0500

    Some improves

--- init.sh
+++ init.sh
@@ -4,11 +4,13 @@ rm -rf web_django
 
 chmod +x init.sh
 chmod +x web/etc/hello.py
-#ln -s /home/box/web/etc/hello.py /home/box/web/
+ln -s /home/box/web/etc/hello.py /home/box/web/
 
 sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo unlink /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
+
+sudo /etc/init.d/mysql start
 
 #sudo ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
 #sudo /etc/init.d/gunicorn restart
@@ -17,4 +19,5 @@ cd /home/box/web/etc/
 #sudo gunicorn -c gunicorn.conf hello:app
 sudo gunicorn --bind='0.0.0.0:8080' hello:app
 
-sudo /etc/init.d/mysql start
+#don't forget to change hello.py!
+
