aac966126323527a23ef8aa9f78e72d2451b3139
shumelsy <sveta@shumel.ru>
2019-07-30 08:28:06 +0500

    Install django project ask and app qa

--- init.sh
+++ init.sh
@@ -3,8 +3,8 @@ mv web_django/init.sh ./
 rm -rf web_django
 
 chmod +x init.sh
-chmod +x web/etc/hello.py
-ln -s /home/box/web/etc/hello.py /home/box/web/
+#chmod +x web/etc/hello.py
+#ln -s /home/box/web/etc/hello.py /home/box/web/
 
 sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo unlink /etc/nginx/sites-enabled/default
@@ -15,9 +15,13 @@ sudo /etc/init.d/mysql start
 #sudo ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
 #sudo /etc/init.d/gunicorn restart
 #sudo /etc/init.d/gunicorn stop
-cd /home/box/web/etc/
+#cd /home/box/web/etc/
 #sudo gunicorn -c gunicorn.conf hello:app
-sudo gunicorn --bind='0.0.0.0:8080' hello:app
+#sudo gunicorn --bind='0.0.0.0:8080' hello:app
+
+cd /home/box/web/ask/
+#sudo gunicorn --bind='0.0.0.0:8000' ask.wsgi:application
+sudo gunicorn -c /home/box/web/etc/gunicorn_ask.conf ask.wsgi:application
 
 #don't forget to change hello.py!
 
