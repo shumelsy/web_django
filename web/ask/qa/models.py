57cd172f8a09001ca1471a0954cd6cf08a88a6b2
shumelsy <sveta@shumel.ru>
2019-08-09 22:51:22 +0500

    Some models

--- web/ask/qa/models.py
+++ web/ask/qa/models.py
@@ -1,6 +1,28 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+class Question(models.Model):
+	objects = QuestionManager()
+	title = models.CharField(max_length=255)
+	text = models.TextField()
+	added_at = models.DateTimeField(blank=True, auto_now_add=True)
+	rating = models.IntegerField(default=0)
+	author = models.ForeignKey(User)
+	likes = models.ManyToManyField(User, related_name='question_like_user')
+
+class Answer(models.Model):
+	text = models.TextField()
+	added_at = models.DateTimeField(blank=True)
+	question = models.OneToOneField(Question)
+	author = models.CharField(max_length=50)
+
+class QuestionManager(models.Manager):
+	def new():
+		return self.order_by('-added_at')
+	def popular():
+		return self.order_by('-rating')
+
+
